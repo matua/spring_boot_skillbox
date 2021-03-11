6b6f36bc608c09179455451021ac7cbd7da91c3c
matua <$9x=4cg^zNNbF&H.UQR_vmN8jSb2U95>
2021-03-11 21:43:30 +0300

    1. application logging 2. Conditional annotation

--- src/main/java/com/example/external/TimeService.java
+++ src/main/java/com/example/external/TimeService.java
@@ -2,6 +2,7 @@ package com.example.external;
 
 import com.example.time_serv.time.TimeProviderProperties;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
 
 import java.text.SimpleDateFormat;
@@ -9,7 +10,8 @@ import java.util.Date;
 import java.util.logging.Logger;
 
 @Service
-public class TimeService {
+@ConditionalOnProperty(value = "spring.profiles.active", havingValue = "prod", matchIfMissing = true)
+public class TimeService implements TimeServiceInterface {
 
     private final TimeProviderProperties timeProviderProperties;
 
@@ -18,6 +20,7 @@ public class TimeService {
         this.timeProviderProperties = timeProviderProperties;
     }
 
+    @Override
     public void printCurrentTime() {
         SimpleDateFormat dateFormat = new SimpleDateFormat(timeProviderProperties.getFormat());
         Logger.getLogger(TimeService.class.getName()).info(timeProviderProperties.getDescription());
--- src/main/java/com/example/time_serv/time/TimeProvider.java
+++ src/main/java/com/example/time_serv/time/TimeProvider.java
@@ -1,6 +1,6 @@
 package com.example.time_serv.time;
 
-import com.example.external.TimeService;
+import com.example.external.TimeServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
@@ -11,22 +11,23 @@ import java.util.logging.Logger;
 @Component
 public class TimeProvider implements CommandLineRunner {
 
-    private final TimeService timeService;
+    private final TimeServiceInterface timeService;
     private final TimeProviderProperties timeProviderProperties;
 
     @Value("${spring.application.name}")
     private String appName;
+
     @Value("${custom.welcomeMessage}")
     private String welcomeMessage;
 
     @Autowired
-    public TimeProvider(TimeService timeService, TimeProviderProperties timeProviderProperties) {
+    public TimeProvider(TimeServiceInterface timeService, TimeProviderProperties timeProviderProperties) {
         this.timeService = timeService;
         this.timeProviderProperties = timeProviderProperties;
     }
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
         Logger.getLogger(TimeProvider.class.getName()).info("running " + appName + " with profile " + timeProviderProperties.getProfile());
         Logger.getLogger(TimeProvider.class.getName()).info(welcomeMessage);
         this.timeService.printCurrentTime();
